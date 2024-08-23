0b7ef2cb266783915eae0cc64c116d22f659e76b
Brian Hicks <brian@brianthicks.com>
2024-08-22 21:45:47 -0500

    fix up test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { init, useDocument } from './lib/use-automerge'
 
-const handle = await init({})
+const handle = await init({ message: 'Howdy' })
 const doc = await useDocument(handle)
 </script>
 
