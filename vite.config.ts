ab43a68615ca634d35e618ba34f4662ebaca33b2
Brian Hicks <brian@brianthicks.com>
2024-08-22 21:28:57 -0500

    set up wasm and top level await

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,12 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import wasm from 'vite-plugin-wasm'
+import topLevelAwait from 'vite-plugin-top-level-await'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools(), wasm(), topLevelAwait()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
