c0b5611a7a0d8e751673940ebc8f7030c5438811
Brian Hicks <brian@brianthicks.com>
2024-08-23 10:30:43 -0500

    initialize with todos

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
+import { type AppV1 } from './lib/types'
 import { init, useDocument } from './lib/use-automerge'
 
-const handle = await init({ message: 'Howdy' })
+const handle = await init<AppV1>({ todos: {} })
 const doc = await useDocument(handle)
 </script>
 
