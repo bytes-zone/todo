add8090822df2d3903bb00eb2eb5d3c31e0c354c
Brian Hicks <brian@brianthicks.com>
2024-08-26 12:39:43 -0500

    test adding todos

--- e2e/vue.spec.ts
+++ e2e/vue.spec.ts
@@ -1,8 +1,56 @@
 import { test, expect } from '@playwright/test'
+import { createMachine } from 'xstate'
+import { createTestModel } from '@xstate/graph'
 
-// See here how to get started:
-// https://playwright.dev/docs/intro
-test('visits the app root url', async ({ page }) => {
-  await page.goto('/')
-  await expect(page.getByText('Add')).toBeVisible()
+const machine = createMachine({
+  initial: 'init',
+  states: {
+    init: {
+      on: {
+        load: 'loaded'
+      }
+    },
+    loaded: {
+      on: {
+        addTodos: 'withTodos'
+      }
+    },
+    withTodos: {}
+  }
+})
+
+const testModel = createTestModel(machine)
+
+const testTodos = ['Clean the carpets', 'Install the new warp core', 'World domination']
+
+testModel.getShortestPaths().forEach((path) => {
+  test(`${path.description}`, async ({ page }) => {
+    await path.test({
+      states: {
+        init: () => {},
+        loaded: async () => {
+          await expect(page.getByText('Add')).toBeVisible()
+        },
+        withTodos: async () => {
+          for (const todo of testTodos) {
+            await expect(page.getByText(todo)).toBeVisible()
+          }
+        },
+        '*': (state) => {
+          throw new Error(`Unimplemented: ${state.value}`)
+        }
+      },
+      events: {
+        load: async () => await page.goto('/'),
+        addTodos: async () => {
+          const form = page.locator('form')
+
+          for (const todo of testTodos) {
+            await form.getByLabel('New Todo').fill(todo)
+            await form.getByRole('button', { name: 'Add' }).click()
+          }
+        }
+      }
+    })
+  })
 })
