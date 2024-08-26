45eb354609f1bd38e78cb22bc45b81a7c1a321d7
Brian Hicks <brian@brianthicks.com>
2024-08-26 07:15:27 -0500

    test init

---
+++ src/lib/ops.spec.ts
@@ -0,0 +1,12 @@
+import { describe, expect, it } from 'vitest'
+import * as ops from './ops'
+
+describe('init', () => {
+  it('should have todos', () => {
+    expect(ops.init().todos).toEqual({})
+  })
+
+  it('should have root todos', () => {
+    expect(ops.init().rootTodos).toEqual({})
+  })
+})
