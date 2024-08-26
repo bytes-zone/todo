2f2bf60adeffbe3efefd675b33cefd95f594c862
Brian Hicks <brian@brianthicks.com>
2024-08-26 11:56:03 -0500

    add daisy

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,10 @@
 import type { Config } from 'tailwindcss'
+import daisyui from 'daisyui'
 
 export default {
   content: ['./src/*.{html,ts,vue}', './src/**/*.{html,ts,vue'],
   theme: {
     extend: {}
   },
-  plugins: []
+  plugins: [daisyui]
 } satisfies Config
