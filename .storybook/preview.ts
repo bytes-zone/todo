e9945a3b5df2a12f6994e95f9d429e973d6c093c
Brian Hicks <brian@brianthicks.com>
2024-08-27 10:03:20 -0500

    does this still need to say dark?

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -14,7 +14,7 @@ const preview: Preview = {
     chromatic: {
       modes: {
         light: { theme: "light" },
-        dark: { theme: "dracula" },
+        dark: { theme: "dark" },
       },
     },
   },
