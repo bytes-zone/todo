d0e519879da895c9b5e54cbcea2d087ac9331f90
Brian Hicks <brian@brianthicks.com>
2024-08-27 16:53:50 -0500

    show eligible todos

--- src/pages/StackBuilder.vue
+++ src/pages/StackBuilder.vue
@@ -1,3 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { handle } from "@/lib/handle"
+import { useDocument } from "@/lib/use-automerge"
+import { computed } from "vue"
 
-<template>TODO</template>
+const doc = await useDocument(handle)
+
+const eligible = computed(() => {
+  return doc.value.rootTodos.map((id) => doc.value.todos[id]).filter((todo) => !todo.completed)
+})
+</script>
+
+<template>
+  <ul>
+    <li v-for="todo in eligible" :key="todo.id">
+      <TodoCompact :todo="todo" />
+    </li>
+  </ul>
+</template>
