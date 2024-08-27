47023f5d68907e087a1e1acdc731e20f745dfad8
Brian Hicks <brian@brianthicks.com>
2024-08-27 06:27:21 -0500

    add story for todocompact

---
+++ src/stories/TodoCompact.stories.ts
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from "@storybook/vue3"
+import { fn } from "@storybook/test"
+import TodoCompact from "@/components/TodoCompact.vue"
+import type { TodoId, TodoV1 } from "@/lib/types"
+
+const todo: TodoV1 = {
+  id: "1234" as TodoId,
+  title: "Test",
+  completed: null,
+  notes: "a brief note",
+  tags: ["personal"],
+  children: [],
+  added: new Date(2024, 1, 1),
+}
+
+const meta = {
+  component: TodoCompact,
+  args: {
+    todo,
+    onToggleComplete: fn(),
+  },
+} satisfies Meta<typeof TodoCompact>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {}
