b609ee8ee53d4328539c7bf975db79ee295c36c5
Brian Hicks <brian@brianthicks.com>
2024-08-27 12:56:30 -0500

    add a header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,38 @@
+<template>
+  <header class="navbar bg-base-100 shadow-xl rounded-box mb-8">
+    <div class="navbar-start">
+      <div class="dropdown">
+        <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h8m-8 6h16"
+            />
+          </svg>
+        </div>
+        <ul
+          tabindex="0"
+          class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+        >
+          <li><RouterLink to="/">Home</RouterLink></li>
+          <li><RouterLink to="/stack">Stack</RouterLink></li>
+        </ul>
+      </div>
+      <a class="btn btn-ghost text-xl">TODO</a>
+    </div>
+    <div class="navbar-end hidden lg:flex">
+      <ul class="menu menu-horizontal px-1">
+        <li><RouterLink to="/">Home</RouterLink></li>
+        <li><RouterLink to="/stack">Stack</RouterLink></li>
+      </ul>
+    </div>
+  </header>
+</template>
