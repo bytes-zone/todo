8563816ea75604ee37320b1fbb00200523c67a04
Brian Hicks <brian@brianthicks.com>
2024-08-28 06:14:49 -0500

    add a basic state machine to do selection logic

---
+++ src/lib/stackBuilderMachine.ts
@@ -0,0 +1,66 @@
+import { assign, emit, enqueueActions, setup } from "xstate"
+import type { AppV1, TodoId } from "./types"
+
+export function eligibleTodos(doc: AppV1): TodoId[] {
+  return doc.rootTodos.filter((id) => {
+    const todo = doc.todos[id]
+    return !todo.completed
+  })
+}
+
+export function startingIndex(doc: AppV1, eligible: TodoId[]): number {
+  return Math.min(...doc.stack.map((id) => eligible.indexOf(id)), eligible.length - 1)
+}
+
+export const stackBuilderMachine = setup({
+  types: {
+    context: {} as {
+      index: number
+      eligible: TodoId[]
+    },
+    input: {} as AppV1,
+    events: {} as { type: "review"; result: "yes" | "no" } | { type: "newDoc"; doc: AppV1 },
+    emitted: {} as { type: "addToStack"; id: TodoId },
+  },
+}).createMachine({
+  context: ({ input }) => {
+    const eligible = eligibleTodos(input)
+    const index = startingIndex(input, eligible)
+
+    return { eligible, index }
+  },
+  on: {
+    newDoc: {
+      actions: assign(({ event, context }) => {
+        const eligible = eligibleTodos(event.doc)
+        return {
+          eligible,
+          index: context.index < 0 ? startingIndex(event.doc, eligible) : context.index,
+        }
+      }),
+      target: ".reviewing",
+    },
+  },
+  initial: "reviewing",
+  states: {
+    reviewing: {
+      always: {
+        guard: ({ context }) => context.index < 0,
+        target: "done",
+      },
+      on: {
+        review: {
+          actions: enqueueActions(({ enqueue, context, event }) => {
+            if (event.result === "yes") {
+              enqueue.emit({ type: "addToStack", id: context.eligible[context.index] })
+            }
+
+            enqueue.assign({ index: ({ context }) => context.index - 1 })
+          }),
+          target: ".",
+        },
+      },
+    },
+    done: {},
+  },
+})
--- src/pages/StackBuilder.vue
+++ src/pages/StackBuilder.vue
@@ -1,19 +1,33 @@
 <script setup lang="ts">
 import { handle } from "@/lib/handle"
+import { stackBuilderMachine } from "@/lib/stackBuilderMachine"
 import { useDocument } from "@/lib/use-automerge"
-import { computed } from "vue"
+import { useMachine } from "@xstate/vue"
+import { watch } from "vue"
 
 const doc = await useDocument(handle)
 
-const eligible = computed(() => {
-  return doc.value.rootTodos.map((id) => doc.value.todos[id]).filter((todo) => !todo.completed)
+const { snapshot, send } = useMachine(stackBuilderMachine, {
+  input: doc.value,
+})
+
+watch(doc, (newDoc) => {
+  send({ type: "newDoc", doc: newDoc })
 })
 </script>
 
 <template>
+  <template v-if="snapshot.matches('reviewing')">
+    <button @click="send({ type: 'review', result: 'yes' })" class="btn btn-neutral">Yes</button>
+    <button @click="send({ type: 'review', result: 'no' })" class="btn btn-neutral">No</button>
+  </template>
+
+  {{ snapshot.context }}
+
   <ul>
-    <li v-for="todo in eligible" :key="todo.id">
-      <TodoCompact :todo="todo" />
+    <li v-for="id in snapshot.context.eligible" :key="id">
+      <template v-if="snapshot.context.eligible[snapshot.context.index] === id">⭐️</template>
+      <TodoCompact :todo="doc.todos[id]" />
     </li>
   </ul>
 </template>
