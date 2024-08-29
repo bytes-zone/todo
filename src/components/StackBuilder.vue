f96dbfa6cfc5dfa5db2b7d7b8ec38e783d48dd1b
Brian Hicks <brian@brianthicks.com>
2024-08-28 21:49:03 -0500

    make the stack builder look at least a little nice

--- src/components/StackBuilder.vue
+++ src/components/StackBuilder.vue
@@ -20,17 +20,30 @@ actorRef.on("addToStack", ({ id }) => emit("addToStack", id))
 </script>
 
 <template>
-  <div v-if="snapshot.matches('reviewing')" class="flex gap-2 mb-2">
-    <button @click="send({ type: 'review', result: 'yes' })" class="btn btn-neutral">Yes</button>
-    <button @click="send({ type: 'review', result: 'no' })" class="btn btn-neutral">No</button>
+  <div v-if="snapshot.matches('reviewing')" class="card w-96 bg-neutral">
+    <div class="card-body items-center text-center">
+      <div class="flex justify-center p-4">
+        <TodoCompact :todo="doc.todos[snapshot.context.eligible[snapshot.context.index]]" />
+      </div>
+      <p class="card-title">Do you want to do this task before the top selected task?</p>
+
+      <div class="card-actions justify-center mt-2">
+        <button @click="send({ type: 'review', result: 'yes' })" class="btn btn-secondary">
+          Yes
+        </button>
+        <button @click="send({ type: 'review', result: 'no' })" class="btn btn-secondary">
+          No
+        </button>
+      </div>
+    </div>
   </div>
 
-  <ul class="flex gap-2 flex-col">
+  <ul class="flex gap-2 flex-col mt-2">
     <li
-      v-for="(id, i) in snapshot.context.eligible.filter((_, i) => i >= snapshot.context.index)"
+      v-for="id in snapshot.context.eligible.filter((_, i) => i > snapshot.context.index)"
       :key="id"
     >
-      <TodoCompact :todo="doc.todos[id]" :highlighted="doc.stack.includes(id)" />
+      <TodoCompact :todo="doc.todos[id]" :highlighted="snapshot.context.selected.includes(id)" />
     </li>
   </ul>
 </template>
