50da7a6ac2acf08b5145fb2646dee39bf73a6430
Brian Hicks <brian@brianthicks.com>
2024-08-28 19:15:20 -0500

    persist adding things to the stack

--- src/pages/StackBuilderPage.vue
+++ src/pages/StackBuilderPage.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import { handle } from "@/lib/handle"
+import { addToStack } from "@/lib/ops"
 import { useDocument } from "@/lib/use-automerge"
 
 const doc = await useDocument(handle)
 </script>
 
 <template>
-  <StackBuilder :doc="doc" />
+  <StackBuilder :doc="doc" @add-to-stack="(id) => handle.change((d) => addToStack(d, id))" />
 </template>
